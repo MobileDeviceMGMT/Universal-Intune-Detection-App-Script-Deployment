38a59735e79a75edf91ab40c5d1ffa2a7fc5ed1b
MobileDeviceMGMT <106992672+MobileDeviceMGMT@users.noreply.github.com>
2025-02-04 09:24:17 +0100

    Update Universal_Detection_Apps_intune.ps1

--- Universal_Detection_Apps_intune.ps1
+++ Universal_Detection_Apps_intune.ps1
@@ -4,23 +4,23 @@ $Apps = @()
 #$Apps += Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" -ErrorAction Ignore 
 $Apps += Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" -ErrorAction Ignore 
 
-$display = $Apps | Where-Object {$PSItem.Displayname -like "Java(TM) SE Development Kit 21*"} | Select DisplayVersion 
+$display = $Apps | Where-Object {$PSItem.Displayname -like "YOUR APP NAME*"} | Select DisplayVersion 
 [version]$version = $display.DisplayVersion
 
-If($version -ge "21.0.5.0")
+If($version -ge "YOUR APP VERSION)
 {
-Write-Host "JAVA JDK $($version) is installed - checked in regedit"
+Write-Host "Application $($version) is installed - checked in regedit"
 exit 0
 }
 else
 {
-Write-Host "JAVA JDK is NOT Installed -checked in regedit"
+Write-Host "Application is NOT Installed -checked in regedit"
 exit 1
 }
 }
 catch
 {
-Write-Host "JAVA JDK is NOT Installed"
+Write-Host "Application is NOT Installed"
 Write-Host "General error during detection method, message: $($_.Exception.Message)"
 exit 1
 }
